684434653d8955fc1d12b936aa8b1db61ec83e06
Gallrax <827269727@qq.com>
2017-10-23 20:46:25 +0800

    增加sql

--- src/test/TestOther.java
+++ src/test/TestOther.java
@@ -20,5 +20,13 @@ public class TestOther {
 
     @Test
     public void test02() {
+        int temp1 = -2147483648;
+        int temp2 = 2147483647;
+        System.out.println(" temp1 bit : " + Integer.toBinaryString(temp1) + ", length : " + Integer.toBinaryString(temp1).length() + ", value : " + temp1);
+        System.out.println(" temp2 bit : " + Integer.toBinaryString(temp2) + ", length : " + Integer.toBinaryString(temp2).length() + ", value : " + temp2);
+        int i1 = temp1 >> 1;
+        int i2 = temp2 >> 1;
+        System.out.println(" i1 bit : " + Integer.toBinaryString(i1) + ", length : " + Integer.toBinaryString(i1).length() + ", value : " + i1);
+        System.out.println(" i2 bit : " + Integer.toBinaryString(i2) + ", length : " + Integer.toBinaryString(i2).length() + ", value : " + i2);
     }
 }
